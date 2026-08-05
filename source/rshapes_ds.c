014ef50ce713a85bf9e06f73ee09ffa08d28b065
Soukie007 <Nmsoukmandjiev007@outlook.com>
2026-08-05 10:48:36 +0300

    changed formatting, drawpoly, not of which functions to changge

--- source/rshapes_ds.c
+++ source/rshapes_ds.c
@@ -6,27 +6,27 @@
 #include <math.h>
 
 
-// Created by Nmsou on 04/08/2026.
-//
-//static Texture2D texShapes = { 1, 1, 1, 1, 7 };                // Texture used on shapes drawing (white pixel loaded by rlgl)
-static Rectangle texShapesRec = { 0.0f, 0.0f, 1.0f, 1.0f };
+
 
 void DrawPixel(int posX, int posY, Color color)
 {
     glPutPixel(posX,posY, RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
-};                                                   // Draw a pixel using geometry [Can be slow, use with care]
+};
 void DrawPixelV(Vector2 position, Color color)
 {
     glPutPixel(position.x,position.y, RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
-};                                                    // Draw a pixel using geometry (Vector version) [Can be slow, use with care]
+};
+
+
+
 void DrawLine(int startPosX, int startPosY, int endPosX, int endPosY, Color color)
 {
     glLine(startPosX,startPosY,endPosX,endPosY, RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
-};                // Draw a line
+};
 void DrawLineV(Vector2 startPos, Vector2 endPos, Color color)
 {
     glLine(startPos.x,startPos.y,endPos.x,endPos.y, RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
-};                                     // Draw a line (using gl lines)
+};
 void DrawLineEx(Vector2 startPos, Vector2 endPos, float thick, Color color)
 {
         //todo dont vibecode
@@ -56,7 +56,7 @@ void DrawLineEx(Vector2 startPos, Vector2 endPos, float thick, Color color)
 
     glTriangleFilled(f32toint(ax), f32toint(ay), f32toint(bx), f32toint(by), f32toint(cx), f32toint(cy), col);
     glTriangleFilled(f32toint(ax), f32toint(ay), f32toint(cx), f32toint(cy), f32toint(ex), f32toint(ey), col);
-}          // Draw a line
+}
 void DrawLineStrip(const Vector2 *points, int pointCount, Color color)
 {
     if (pointCount < 2) return; // Security check
@@ -65,10 +65,9 @@ void DrawLineStrip(const Vector2 *points, int pointCount, Color color)
     for (int i = 0; i < pointCount - 1; i++)
     {
 
-        DrawLineV(points[i],points[i + i],color);
+        DrawLineV(points[i],points[i + 1],color);
     }
-};                            // Draw lines sequence (using gl lines)
-
+};
 void DrawLineDashed(Vector2 startPos, Vector2 endPos, int dashSize, int spaceSize, Color color)
 {
     float dx = endPos.x - startPos.x;
@@ -103,7 +102,10 @@ void DrawLineDashed(Vector2 startPos, Vector2 endPos, int dashSize, int spaceSiz
 
         traveled += (float)patternLen;
     }
-}// Draw a dashed line
+}
+
+
+
 void DrawCircle(int centerX, int centerY, float radius, Color color)
 {
     //todo dont vibecode
@@ -130,10 +132,6 @@ void DrawCircleV(Vector2 center, float radius, Color color)
 {
     DrawCircle(center.x,center.y,radius,color);
 };
-/*
-
-void DrawCircleSector(Vector2 center, float radius, float startAngle, float endAngle, int segments, Color color); // Draw a piece of a circle
-void DrawCircleSectorLines(Vector2 center, float radius, float startAngle, float endAngle, int segments, Color color); // Draw circle sector outline*/
 void DrawCircleLines(int centerX, int centerY, float radius, Color color)
 {
     int x = 0;
@@ -162,43 +160,25 @@ void DrawCircleLines(int centerX, int centerY, float radius, Color color)
             d = d + 4 * (x - y) + 10;
         }
     }
-} // Draw circle outline
+}
 void DrawCircleLinesV(Vector2 center, float radius, Color color)
 {
     DrawCircleLines(center.x,center.y,radius,color);
-};                                // Draw circle outline (Vector version)
-
+};
 void DrawCircleGradient(Vector2 center, float radius, Color inner, Color outer)
 {
 
-    s32 radiusF32 = floattof32(radius); //
+    s32 radiusF32 = floattof32(radius); //todo dont vibecode
 
-    u16 innerCol = RGB15(inner.r >> 3, inner.g >> 3, inner.b >> 3);
-    u16 outerCol = RGB15(outer.r >> 3, outer.g >> 3, outer.b >> 3);
-
-    s16 prevAngle = 0;
-    int prevX = (int)center.x + f32toint(mulf32(radiusF32, cosLerp(prevAngle)));
-    int prevY = (int)center.y + f32toint(mulf32(radiusF32, sinLerp(prevAngle)));
-
-    for (int i = 1; i <= 36; i++)
-    {
-        s16 angle = (s16)((i * DEGREES_IN_CIRCLE) / 36);
-
-        int x = (int)center.x + f32toint(mulf32(radiusF32, cosLerp(angle)));
-        int y = (int)center.y + f32toint(mulf32(radiusF32, sinLerp(angle)));
-
-        glTriangleFilledGradient((int)center.x, (int)center.y, prevX, prevY, x, y, innerCol, outerCol, outerCol);
-
-        prevX = x;
-        prevY = y;
-    }
 }
+/*void DrawCircleSector(Vector2 center, float radius, float startAngle, float endAngle, int segments, Color color); // Draw a piece of a circle
+void DrawCircleSectorLines(Vector2 center, float radius, float startAngle, float endAngle, int segments, Color color); // Draw circle sector outline*/
+
 
 
 void DrawEllipseV(Vector2 center, float radiusH, float radiusV, Color color)
 {
-    const int segments = 36; // matches raylib's original 360/10 = 36 steps
-
+/*
     s32 radiusHF32 = floattof32(radiusH);
     s32 radiusVF32 = floattof32(radiusV);
     int cx = (int)center.x;
@@ -210,9 +190,9 @@ void DrawEllipseV(Vector2 center, float radiusH, float radiusV, Color color)
     int prevX = cx + f32toint(mulf32(radiusHF32, cosLerp(prevAngle)));
     int prevY = cy + f32toint(mulf32(radiusVF32, sinLerp(prevAngle)));
 
-    for (int i = 1; i <= segments; i++)
+    for (int i = 1; i <= 36; i++)
     {
-        s16 angle = (s16)((i * DEGREES_IN_CIRCLE) / segments);
+        s16 angle = (s16)((i * DEGREES_IN_CIRCLE) / 36);
 
         int x = cx + f32toint(mulf32(radiusHF32, cosLerp(angle)));
         int y = cy + f32toint(mulf32(radiusVF32, sinLerp(angle)));
@@ -220,45 +200,48 @@ void DrawEllipseV(Vector2 center, float radiusH, float radiusV, Color color)
         glTriangleFilled(cx, cy, prevX, prevY, x, y, col);
 
         prevX = x;
-        prevY = y;
-    }
+        prevY = y;*/ //todo dont vibecode
+
 }
-// Draw ellipse
 void DrawEllipse(int centerX,int centerY, float radiusH, float radiusV, Color color)
 {
     DrawEllipseV((Vector2){centerX,centerY},radiusH,radiusV,color);
-};                      // Draw ellipse (Vector version)
-void DrawEllipseLines(int centerX, int centerY, float radiusH, float radiusV, Color color);// Draw ellipse outline
-void DrawEllipseLinesV(Vector2 center, float radiusH, float radiusV, Color color);                 // Draw ellipse outline (Vector version)
-void DrawRing(Vector2 center, float innerRadius, float outerRadius, float startAngle, float endAngle, int segments, Color color); // Draw ring
-void DrawRingLines(Vector2 center, float innerRadius, float outerRadius, float startAngle, float endAngle, int segments, Color color); // Draw ring outline
+};
+void DrawEllipseLines(int centerX, int centerY, float radiusH, float radiusV, Color color);
+void DrawEllipseLinesV(Vector2 center, float radiusH, float radiusV, Color color);
+
+
+
+void DrawRing(Vector2 center, float innerRadius, float outerRadius, float startAngle, float endAngle, int segments, Color color);
+void DrawRingLines(Vector2 center, float innerRadius, float outerRadius, float startAngle, float endAngle, int segments, Color color);
+
 
 
 void DrawRectangle(int posX, int posY, int width, int height, Color color)
 {
     glBoxFilled(posX,posY,posX + width, posY + height,RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
-};                        // Draw a color-filled rectangle
+};
 void DrawRectangleV(Vector2 position, Vector2 size, Color color)
 {
     glBoxFilled(position.x,position.y,position.x + size.x,position.y + size.y,RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
-};                                  // Draw a color-filled rectangle (Vector version)
+};
 void DrawRectangleRec(Rectangle rec, Color color)
 {
     glBoxFilled(rec.x,rec.y,rec.x + rec.width,rec.y + rec.height,RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
-};                                                 // Draw a color-filled rectangle
-void DrawRectanglePro(Rectangle rec, Vector2 origin, float rotation, Color color);                 // Draw a color-filled rectangle with pro parameters
+};
+void DrawRectanglePro(Rectangle rec, Vector2 origin, float rotation, Color color);
 void DrawRectangleGradientV(int posX, int posY, int width, int height, Color top, Color bottom)
 {
     int coltop = RGB15(top.r >> 3, top.g >> 3, top.b >> 3);
     int colbot = RGB15(bottom.r >> 3, bottom.g >> 3, bottom.b >> 3);
     glBoxFilledGradient(posX,posY,posX + width, posY + height,coltop,colbot,coltop,colbot);
-};   // Draw a vertical-gradient-filled rectangle
+};
 void DrawRectangleGradientH(int posX, int posY, int width, int height, Color left, Color right)
 {
     int coleft = RGB15(left.r >> 3, left.g >> 3, left.b >> 3);
     int colright = RGB15(right.r >> 3, right.g >> 3, right.b >> 3);
     glBoxFilledGradient(posX,posY,posX + width, posY + height,coleft,coleft,colright,colright);
-};   // Draw a horizontal-gradient-filled rectangle
+};
 void DrawRectangleGradientEx(Rectangle rec, Color topLeft, Color bottomLeft, Color bottomRight, Color topRight)
 {
     int col1 = RGB15(topLeft.r >> 3, topLeft.g >> 3, topLeft.b >> 3);
@@ -266,36 +249,110 @@ void DrawRectangleGradientEx(Rectangle rec, Color topLeft, Color bottomLeft, Col
     int col3 = RGB15(bottomRight.r >> 3, bottomRight.g >> 3, bottomRight.b >> 3);;
     int col4 = RGB15(topRight.r >> 3, topRight.g >> 3, topRight.b >> 3);
     glBoxFilledGradient(rec.x,rec.y,rec.x + rec.width,rec.y + rec.height,col1,col2,col3,col4);
-}; // Draw a gradient-filled rectangle with custom vertex colors
+};
 void DrawRectangleLines(int posX, int posY, int width, int height, Color color)
 {
-    glBox(posX,posY,posX + width,posY + height, RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
-};                   // Draw rectangle outline
+    glBox(posX,posY + height,posX + width,posY , RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
+};
 void DrawRectangleLinesEx(Rectangle rec, float lineThick, Color color)
 {
     //TODO IMPLEMENT
-};                       // Draw rectangle outline with extended parameters
+};
 void DrawRectangleRoundedLines(Rectangle rec, float roundness, int segments, Color color)
 {
     DrawLine(rec.x + 1, rec.y, rec.x + rec.width - 1, rec.y, color);
     DrawLine(rec.x + rec.width, rec.y + 1, rec.x + rec.width, rec.y + rec.height - 1, color);
     DrawLine(rec.x + 1, rec.y + rec.height, rec.x + rec.width - 1, rec.y + rec.height, color);
     DrawLine(rec.x, rec.y + 1, rec.x, rec.y + rec.height - 1, color);
-}        // Draw rectangle with rounded edges// Draw rectangle lines with rounded edges
+}
 void DrawRectangleRoundedLinesEx(Rectangle rec, float roundness, int segments, float lineThick, Color color)
 {
     //TODO IMPLEMENT
-}; // Draw rectangle with rounded edges outline
+};
+
+
+
 void DrawTriangle(Vector2 v1, Vector2 v2, Vector2 v3, Color color)
 {
     glTriangleFilled(v1.x,v1.y,v2.x,v2.y,v3.x,v3.y,RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
-};                                // Draw a color-filled triangle (vertex in counter-clockwise order!)
+};
 void DrawTriangleLines(Vector2 v1, Vector2 v2, Vector2 v3, Color color)
 {
     glTriangle(v1.x,v1.y,v2.x,v2.y,v3.x,v3.y,RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
-};                           // Draw triangle outline (vertex in counter-clockwise order!)
-void DrawTriangleFan(const Vector2 *points, int pointCount, Color color);                          // Draw a triangle fan defined by points (first vertex is the center)
-void DrawTriangleStrip(const Vector2 *points, int pointCount, Color color);                        // Draw a triangle strip defined by points
-void DrawPoly(Vector2 center, int sides, float radius, float rotation, Color color);               // Draw a regular polygon (Vector version)
-void DrawPolyLines(Vector2 center, int sides, float radius, float rotation, Color color);          // Draw a polygon outline of n sides
-void DrawPolyLinesEx(Vector2 center, int sides, float radius, float rotation, float lineThick, Color color); // Draw a polygon outline of n sides with extended parameters
+};
+void DrawTriangleFan(const Vector2 *points, int pointCount, Color color)
+{
+    if (pointCount < 3) return;
+
+    for (int i = 1; i < pointCount - 1; i++)
+    {
+
+        glTriangleFilled((int)points[0].x, (int) points[0].y, (int)points[i].x, (int)points[i].y, (int)points[i + 1].x, (int)points[i + 1].y, RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
+    }
+}
+void DrawTriangleStrip(const Vector2 *points, int pointCount, Color color)
+{
+    if (pointCount >= 3)
+    {
+
+        for (int i = 2; i < pointCount; i++)
+        {
+            if ((i%2) == 0)
+            {
+                glTriangleFilled((int)points[i].x, (int)points[i].y,(int)points[i - 2].x, (int)points[i - 2].y,(int)points[i - 1].x, (int)points[i - 1].y,RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
+            }
+            else
+            {
+                glTriangleFilled((int)points[i].x,(int)points[i].y,(int)points[i - 1].x,(int)points[i - 1].y,(int)points[i - 2].x,(int)points[i - 2].y,RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
+            }
+        }
+    }
+}
+
+
+
+void DrawPoly(Vector2 center, int sides, float radius, float rotation, Color color)
+{
+    if (sides < 3) sides = 3;
+
+    s32 radiusF32 = floattof32(radius);
+    int cx = (int)center.x;
+    int cy = (int)center.y;
+
+    s16 centralAngle = (s16)(((int)rotation * DEGREES_IN_CIRCLE) / 360);
+    s16 angleStep = (s16)(DEGREES_IN_CIRCLE / sides);
+
+
+    for (int i = 0; i < sides; i++)
+    {
+        s16 a1 = centralAngle + (i * angleStep);
+        s16 a2 = a1 + angleStep;
+
+        int x1 = cx + f32toint(mulf32(radiusF32, cosLerp(a1)));
+        int y1 = cy + f32toint(mulf32(radiusF32, sinLerp(a1)));
+        int x2 = cx + f32toint(mulf32(radiusF32, cosLerp(a2)));
+        int y2 = cy + f32toint(mulf32(radiusF32, sinLerp(a2)));
+
+        glTriangleFilled(cx, cy, x1, y1, x2, y2, RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
+    }
+}
+
+void DrawPolyLines(Vector2 center, int sides, float radius, float rotation, Color color)
+{
+    if (sides < 3) sides = 3;
+    s32 radiusF32 = floattof32(radius);
+    s16 centralAngle = (s16)(((int)rotation * DEGREES_IN_CIRCLE) / 360);
+    s16 angleStep = (s16)(DEGREES_IN_CIRCLE / sides);
+
+    for (int i = 0; i < sides; i++)
+    {
+        int x1 = (int)(center.x) + f32toint(mulf32(cosLerp(centralAngle),radiusF32));
+        int x2 = (int )(center.x) + f32toint(mulf32(cosLerp(centralAngle + angleStep),radiusF32));
+
+        int y1 = (int)(center.y) + f32toint(mulf32(sinLerp(centralAngle),radiusF32));
+        int y2 = (int)(center.y) + f32toint(mulf32(sinLerp(centralAngle + angleStep),radiusF32));
+        glLine(x1,y1,x2,y2,RGB15(color.r >> 3, color.g >> 3, color.b >> 3));
+        centralAngle += angleStep;
+
+    }
+}
